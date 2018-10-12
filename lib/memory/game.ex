5c8869fa7bd0c1a7951ba91ac7463f39f5c8d6f4
Emily Hontoria <hontoria@ccs.neu.edu>
2018-10-11 20:22:25 -0400

    Flip updates

--- lib/memory/game.ex
+++ lib/memory/game.ex
@@ -24,39 +24,63 @@ defmodule Memory.Game do
 
   def add_player(game, user) do
     put_in(game, [:players, user], 0)
+    if (!game[:turn]) do
+      put_in(game, [:turn], user)
+    end
   end
 
-  def flip(%{tiles: tiles, guesses: guesses, partial_guess: pg, players: players, turn: t}, new_guess) do
-    if pg do
-      is_correct = Board.same_letter(tiles, pg, new_guess)
-      if is_correct do
-        tile_state = %{ completed: true }
-        tiles = tiles
-                |> Board.update_tile(pg, tile_state)
-                |> Board.update_tile(new_guess, tile_state)
-        {%{tiles: tiles,
-          guesses: [{pg, new_guess}|guesses],
-          partial_guess: nil}, nil}
-      else
-        tile_state = %{ visible: true }
-        visible_tiles = tiles
-                |> Board.update_tile(pg, tile_state)
-                |> Board.update_tile(new_guess, tile_state)
-        tile_state = %{ visible: false }
-        hidden_tiles = tiles
-                |> Board.update_tile(pg, tile_state)
-                |> Board.update_tile(new_guess, tile_state)
-        {%{tiles: hidden_tiles,
-          guesses: [{pg, new_guess}|guesses],
-          partial_guess: nil},
-          %{tiles: visible_tiles,
-          guesses: [{pg, new_guess}|guesses],
-          partial_guess: nil}}
-      end
+  def next_turn(players, user) do
+    player_keys = Map.keys(players)
+    if (player_keys[0] == user) do
+      player_keys[1]
     else
-      {%{tiles: Board.update_tile(tiles, new_guess, %{ visible: true }),
-        guesses: guesses,
-        partial_guess: new_guess}, nil}
+      player_keys[0]
+    end
+  end
+
+  def flip(%{tiles: tiles, guesses: guesses, partial_guess: pg, players: players, turn: t}, new_guess, user) do
+    if (user == t) do
+      if pg do
+        is_correct = Board.same_letter(tiles, pg, new_guess)
+        if is_correct do
+          tile_state = %{ completed: true }
+          new_score = players[user] + 1
+          new_players = put_in(players, [user], new_score)
+          tiles = tiles
+                  |> Board.update_tile(pg, tile_state)
+                  |> Board.update_tile(new_guess, tile_state)
+          {%{tiles: tiles,
+            guesses: [{pg, new_guess}|guesses],
+            partial_guess: nil,
+            players: new_players,
+            turn: next_turn(players, t)}, nil}
+        else
+          tile_state = %{ visible: true }
+          visible_tiles = tiles
+                  |> Board.update_tile(pg, tile_state)
+                  |> Board.update_tile(new_guess, tile_state)
+          tile_state = %{ visible: false }
+          hidden_tiles = tiles
+                  |> Board.update_tile(pg, tile_state)
+                  |> Board.update_tile(new_guess, tile_state)
+          {%{tiles: hidden_tiles,
+            guesses: [{pg, new_guess}|guesses],
+            partial_guess: nil,
+            players: players,
+            turn: next_turn(players, t)},
+            %{tiles: visible_tiles,
+            guesses: [{pg, new_guess}|guesses],
+            partial_guess: nil,
+            players: players,
+            turn: next_turn(players, t)}}
+        end
+      else
+        {%{tiles: Board.update_tile(tiles, new_guess, %{ visible: true }),
+          guesses: guesses,
+          partial_guess: new_guess,
+          players: players,
+          turn: t}, nil}
+      end
     end
   end
 
